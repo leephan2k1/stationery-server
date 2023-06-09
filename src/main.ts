1e6ea66edc8c6e4eba0db66ecc5898dc4944c021
leephan2k1 <pttl2k1@gmail.com>
2023-06-09 15:08:06 +0700

    feat: 🎸 enable cors

--- src/main.ts
+++ src/main.ts
@@ -18,6 +18,11 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: process.env.CLIENT_URL || `http://localhost:4200`,
+    credentials: true,
+  });
+
   //should be call before SwaggerModule setup
   app.setGlobalPrefix('api');
   app.enableVersioning({
